90f12269d2e78c30e67c1161a139bab9116badad
Marcus Loren <MarcusLoren@users.noreply.huggingface.co>
2024-06-08 04:48:42 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import torch  
+import time
 from meshgpt_pytorch import ( 
     MeshTransformer,
     mesh_render
@@ -9,6 +10,7 @@ import gradio as gr
 import numpy as np 
 
 transformer = MeshTransformer.from_pretrained("MarcusLoren/MeshGPT-preview")
+total_generations = 0
 
 def save_as_obj(file_path):
     v, f = igl.read_triangle_mesh(file_path)
@@ -22,6 +24,10 @@ def predict(text, num_input, num_temp):
     transformer.eval()
     labels = [label.strip() for label in text.split(',')] 
     output = []
+    total_generations +=1 
+    current_time = time.time() 
+    formatted_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(current_time))
+    print(formatted_time, " Input:", text, "num_input", num_input, "num_temp",num_temp , "total_generations", total_generations)
     if num_input > 1:
         for label in labels:
             output.append((transformer.generate(texts = [label ] * num_input, temperature = num_temp)))   
