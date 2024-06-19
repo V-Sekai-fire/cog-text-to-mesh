0fa316dcf0dad01ea4e581413bbcbfdec3962afd
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2024-06-18 17:03:32 -0700

    Convert to cog.

--- README.md
+++ README.md
@@ -10,4 +10,16 @@ pinned: false
 license: mit
 ---
 
-Check out the configuration reference at https://huggingface.co/docs/hub/spaces-config-reference
+```bash
+cog predict -i text="cat"
+```
+
+Currently, this project is in the **Sandbox** phase, which is the initial stage of a project according to the V-Sekai maturity model.
+
+The V-Sekai maturity model consists of three stages:
+
+1. **Sandbox**: This stage serves as the starting point for projects, aiming to nurture innovation and project development.
+2. **Incubating**: At this stage, projects are on their journey towards graduation, with an expectation of having a stable codebase and well-structured formation.
+3. **Graduated**: These projects have reached maturity, adopting best practices and catering to a broad user base.
+
+As the project fulfills the criteria laid out by V-Sekai, it will progress through these stages.
---
+++ predict.py
@@ -0,0 +1,72 @@
+from cog import BasePredictor, Input, File
+import torch
+import time
+from meshgpt_pytorch import MeshTransformer, mesh_render
+import igl
+import numpy as np
+
+class Predictor(BasePredictor):
+    def setup(self):
+        """Load the model into memory to make running multiple predictions efficient"""
+        self.transformer = MeshTransformer.from_pretrained(
+            "MarcusLoren/MeshGPT-preview"
+        )
+
+    def save_as_obj(self, file_path):
+        v, f = igl.read_triangle_mesh(file_path)
+        v, f, _, _ = igl.remove_unreferenced(v, f)
+        c, _ = igl.orientable_patches(f)
+        f, _ = igl.orient_outward(v, f, c)
+        igl.write_triangle_mesh(file_path, v, f)
+        return file_path
+
+    def predict(
+        self,
+        text: str = Input(description="Enter labels, separated by commas"),
+        num_input: int = Input(description="Number of examples per input", default=1),
+        num_temp: float = Input(description="Temperature (0 to 1)", default=0),
+    ) -> File:
+        """Run a single prediction on the model"""
+        self.transformer.eval()
+        labels = [label.strip() for label in text.split(",")]
+
+        output = []
+        current_time = time.time()
+
+        formatted_time = time.strftime(
+            "%Y-%m-%d %H:%M:%S", time.localtime(current_time)
+        )
+        print(
+            formatted_time,
+            " Input:",
+            text,
+            "num_input",
+            num_input,
+            "num_temp",
+            num_temp,
+        )
+
+        if num_input > 1:
+            for label in labels:
+                output.append(
+                    (
+                        self.transformer.generate(
+                            texts=[label] * num_input, temperature=num_temp
+                        )
+                    )
+                )
+        else:
+            output.append(
+                (self.transformer.generate(texts=labels, temperature=num_temp))
+            )
+
+        mesh_render.save_rendering("./render.obj", output)
+
+        return self.save_as_obj("./render.obj")
+
+
+if __name__ == "__main__":
+    predictor = Predictor()
+    predictor.setup()
+
+    print(predictor.predict("cat", 1, 0.0))
